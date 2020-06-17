8cab4a479feb0335b055ebea5c7f34299e701fcc
Andrey Kudryavtsev <lomastering@gmail.com>
2020-06-18 00:09:22 +0300

    feat: advanced functions

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,24 @@ const currentOutput = document.querySelector('.calculator__screen');
 const numberButtons = document.querySelectorAll('.button_type_number');
 const operationButtons = document.querySelectorAll('.button_type_operation');
 const resetButton = document.querySelector('.button_type_reset');
+const resultButton = document.querySelector('.button_type_result');
+const advancedButtons = document.querySelectorAll('.button_type_advanced');
 const defaultResult = 0;
 
-let resultBeforeCalc = defaultResult;
-let currentResult = defaultResult;
+let firstValue = defaultResult;
+let secondValue = defaultResult;
+let finalResult = defaultResult;
 let operator = null;
 let currentOperator = null;
+let log = '0';
 
 const getNumber = () => currentOutput.value;
 const setNumber = value => currentOutput.value = value.toString().slice(0, 24);
 
 const printNumber = (numb) => {
   let output = getNumber();
-  // Disabling adding to input is the result is shown or Error
-  if (currentResult || output === 'Error') {
+  // Reset on error or new calculation
+  if (output === 'Error' || finalResult && !currentOperator) {
     reset();
     output = '';
   }
@@ -25,7 +29,7 @@ const printNumber = (numb) => {
     currentOperator = null;
     output = '';
   }
-  // Only one decimal point allowed, removing front zero
+  // Only one decimal point allowed, removing leading zero
   if (numb === '.') {
     if (output.indexOf(numb) !== -1) return;
   } else if (output === '0') output = '';
@@ -35,19 +39,17 @@ const printNumber = (numb) => {
 
 const addOperator = (sign) => {
   if (getNumber() === 'Error') return;
-  if (sign !== 'RESULT') currentOperator = sign;
-  // Calculate if two numbers were provided
-  if (operator) {
-    if (!currentResult) currentResult = parseFloat(getNumber());
-    // Error when dividing by 0
-    if (operator === 'DIVIDE' && currentResult === 0) {
-      setNumber('Error');
-      return;
-    }
-    let finalResult = calculate(resultBeforeCalc, currentResult, operator);
-    setNumber(finalResult);
+  // Setting up first number
+  if (!firstValue) firstValue = parseFloat(getNumber());
+  // Operation pressed more than once
+  else if (!finalResult) {
+    secondValue = parseFloat(getNumber());
+    showResult();
   }
-  resultBeforeCalc = parseFloat(getNumber());
+  // Setting up signal for continuing equation
+  currentOperator = sign;
+  // Resetting equals button behaviour
+  finalResult = defaultResult;
 }
 
 const calculate = (firstNum, secondNum, sign) => {
@@ -57,25 +59,73 @@ const calculate = (firstNum, secondNum, sign) => {
     case 'SUBTRACT':
       return firstNum - secondNum;
     case 'MULTIPLY':
-      return math.multiply(math.bignumber(firstNum), math.bignumber(secondNum));
+      // Floating point precision fix
+      return +(math.multiply(math.bignumber(firstNum), math.bignumber(secondNum)));
     case 'DIVIDE':
-      return math.divide(math.bignumber(firstNum), math.bignumber(secondNum));
+      return +(math.divide(math.bignumber(firstNum), math.bignumber(secondNum)));
   }
 }
 
+const showResult = () => {
+  // Setting up second number if operation pressed more than once
+  if(!finalResult) secondValue = parseFloat(getNumber());
+  // Forbid dividing by 0
+  if (operator === 'DIVIDE' && secondValue === 0) {
+    setNumber('Error');
+    return;
+  }
+  if (firstValue && secondValue && operator) {
+    finalResult = calculate(firstValue, secondValue, operator);
+    setNumber(finalResult);
+    // Changing the result for following calculations
+    firstValue = parseFloat(getNumber());
+  }
+}
+
+const modifyValue = (action) => {
+  const currentValue = getNumber();
+  let newValue;
+  if (currentValue) {
+    switch (action) {
+      case 'NEGATE':
+        newValue = -currentValue;
+        // Adding to memory
+        if (firstValue) firstValue = newValue;
+        break;
+      case 'SQUARE':
+        // Prevent for negative numbers
+        if (currentValue >= 0) {
+          newValue = +(math.sqrt(math.bignumber(currentValue)));
+          //TODO Prevent adding to the current value
+          // finalResult = newValue;
+        } else {
+          setNumber('Error');
+          return;
+        }
+        break;
+      case 'PERCENT':
+        newValue = +(math.multiply(math.bignumber(currentValue), .01));
+        //TODO Prevent adding to the current value
+        // finalResult = newValue;
+        break;
+      case 'BACKSPACE':
+        newValue = currentValue.slice(0, -1);
+        // Adding to memory
+        if (firstValue) firstValue = newValue;
+        break;
+    }
+  }
+  setNumber(newValue);
+}
+
 const reset = () => {
-  resultBeforeCalc = defaultResult;
-  currentResult = defaultResult;
+  firstValue = defaultResult;
+  secondValue = defaultResult;
+  finalResult = defaultResult;
   operator = null;
   currentOperator = null;
   currentOutput.value = defaultResult;
 }
-
-/* const disableOperators = () => {
-  for (let el of operationButtons) {
-    el.removeEventListener('click', addOperator.bind(this, el.value));
-  };
-} */
 
 for (let el of numberButtons) {
   el.addEventListener('click', printNumber.bind(this, el.textContent));
@@ -84,5 +134,10 @@ for (let el of operationButtons) {
   let binder = addOperator.bind(this, el.value);
   el.addEventListener('click', binder);
 };
+for (let el of advancedButtons) {
+  let binder = modifyValue.bind(this, el.value);
+  el.addEventListener('click', binder);
+};
 resetButton.addEventListener('click', reset);
+resultButton.addEventListener('click', showResult);
 
