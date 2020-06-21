59104e547a6fd096841bdf79c8738c94fb4812a6
Andrey Kudryavtsev <lomastering@gmail.com>
2020-06-21 21:11:49 +0300

    feat: memory functions

--- scripts/script.js
+++ scripts/script.js
@@ -4,22 +4,26 @@ const operationButtons = document.querySelectorAll('.button_type_operation');
 const resetButton = document.querySelector('.button_type_reset');
 const resultButton = document.querySelector('.button_type_result');
 const advancedButtons = document.querySelectorAll('.button_type_advanced');
-const defaultResult = 0;
+const memoryButtons = document.querySelectorAll('.button_type_memory');
 
-let firstValue = defaultResult;
-let secondValue = defaultResult;
-let finalResult = defaultResult;
+const buttons = { operationButtons, advancedButtons, memoryButtons };
+
+let firstValue = null;
+let secondValue = null;
+let finalResult = null;
 let operator = null;
 let currentOperator = null;
-let log = '0';
+let memory = 0;
+let memoryAdded = false;
 
 const getNumber = () => currentOutput.value;
-const setNumber = value => currentOutput.value = value.toString().slice(0, 24);
+const setNumber = value =>
+  (currentOutput.value = value.toString().slice(0, 24));
 
-const printNumber = (numb) => {
+const printNumber = numb => {
   let output = getNumber();
   // Reset on error or new calculation
-  if (output === 'Error' || finalResult && !currentOperator) {
+  if (output === 'Error' || (finalResult && !currentOperator)) {
     reset();
     output = '';
   }
@@ -29,15 +33,21 @@ const printNumber = (numb) => {
     currentOperator = null;
     output = '';
   }
+  // Disable adding to input after memory operation
+  if (memoryAdded) {
+    memoryAdded = false;
+    output = '';
+  }
   // Only one decimal point allowed, removing leading zero
   if (numb === '.') {
     if (output.indexOf(numb) !== -1) return;
   } else if (output === '0') output = '';
 
-  setNumber(output += numb);
-}
+  setNumber((output += numb));
+};
 
-const addOperator = (sign) => {
+//TODO Disable buttons on error
+const addOperator = sign => {
   if (getNumber() === 'Error') return;
   // Setting up first number
   if (!firstValue) firstValue = parseFloat(getNumber());
@@ -49,8 +59,8 @@ const addOperator = (sign) => {
   // Setting up signal for continuing equation
   currentOperator = sign;
   // Resetting equals button behaviour
-  finalResult = defaultResult;
-}
+  finalResult = null;
+};
 
 const calculate = (firstNum, secondNum, sign) => {
   switch (sign) {
@@ -60,29 +70,36 @@ const calculate = (firstNum, secondNum, sign) => {
       return firstNum - secondNum;
     case 'MULTIPLY':
       // Floating point precision fix
-      return +(math.multiply(math.bignumber(firstNum), math.bignumber(secondNum)));
+      return +math.multiply(
+        math.bignumber(firstNum),
+        math.bignumber(secondNum)
+      );
     case 'DIVIDE':
-      return +(math.divide(math.bignumber(firstNum), math.bignumber(secondNum)));
+      return +math.divide(math.bignumber(firstNum), math.bignumber(secondNum));
   }
-}
+};
 
 const showResult = () => {
   // Setting up second number if operation pressed more than once
-  if(!finalResult) secondValue = parseFloat(getNumber());
+  if (!finalResult) secondValue = parseFloat(getNumber());
   // Forbid dividing by 0
   if (operator === 'DIVIDE' && secondValue === 0) {
     setNumber('Error');
     return;
   }
-  if (firstValue && secondValue && operator) {
+  if (
+    (firstValue || firstValue === 0) &&
+    (secondValue || secondValue === 0) &&
+    operator
+  ) {
     finalResult = calculate(firstValue, secondValue, operator);
     setNumber(finalResult);
     // Changing the result for following calculations
     firstValue = parseFloat(getNumber());
   }
-}
+};
 
-const modifyValue = (action) => {
+const modifyValue = action => {
   const currentValue = getNumber();
   let newValue;
   if (currentValue) {
@@ -95,7 +112,7 @@ const modifyValue = (action) => {
       case 'SQUARE':
         // Prevent for negative numbers
         if (currentValue >= 0) {
-          newValue = +(math.sqrt(math.bignumber(currentValue)));
+          newValue = +math.sqrt(math.bignumber(currentValue));
           //TODO Prevent adding to the current value
           // finalResult = newValue;
         } else {
@@ -104,40 +121,77 @@ const modifyValue = (action) => {
         }
         break;
       case 'PERCENT':
-        newValue = +(math.multiply(math.bignumber(currentValue), .01));
+        newValue = +math.multiply(math.bignumber(currentValue), 0.01);
         //TODO Prevent adding to the current value
         // finalResult = newValue;
         break;
       case 'BACKSPACE':
-        newValue = currentValue.slice(0, -1);
-        // Adding to memory
+        if (currentValue.length > 1) {
+          newValue = currentValue.slice(0, -1);
+          // Adding to memory
+        } else newValue = 0;
         if (firstValue) firstValue = newValue;
         break;
     }
   }
   setNumber(newValue);
-}
+};
+
+//TODO Fix double memory recall
+const accessMemory = value => {
+  switch (value) {
+    case 'MPLUS':
+      memory += parseFloat(getNumber());
+      memoryAdded = true;
+      break;
+    case 'MMINUS':
+      memory -= parseFloat(getNumber());
+      memoryAdded = true;
+      break;
+    case 'MRC':
+      setNumber(memory);
+      if (!secondValue) {
+        secondValue = memory;
+        operator = currentOperator;
+        currentOperator = null;
+        finalResult = memory;
+      }
+  }
+};
 
 const reset = () => {
-  firstValue = defaultResult;
-  secondValue = defaultResult;
-  finalResult = defaultResult;
+  firstValue = null;
+  secondValue = null;
+  finalResult = null;
   operator = null;
   currentOperator = null;
-  currentOutput.value = defaultResult;
-}
+  currentOutput.value = 0;
+};
 
-for (let el of numberButtons) {
-  el.addEventListener('click', printNumber.bind(this, el.textContent));
+const switchButtons = () => {
+  for (const arr in buttons) {
+    let func;
+    for (const el of buttons[arr]) {
+      switch (arr) {
+        case 'operationButtons':
+          func = addOperator;
+          break;
+        case 'advancedButtons':
+          func = modifyValue;
+          break;
+        case 'memoryButtons':
+          func = accessMemory;
+          break;
+      }
+      const binder = func.bind(this, el.value);
+      el.addEventListener('click', binder);
+    }
+  }
 };
-for (let el of operationButtons) {
-  let binder = addOperator.bind(this, el.value);
-  el.addEventListener('click', binder);
-};
-for (let el of advancedButtons) {
-  let binder = modifyValue.bind(this, el.value);
-  el.addEventListener('click', binder);
-};
+
+for (const el of numberButtons) {
+  el.addEventListener('click', printNumber.bind(this, el.value));
+}
 resetButton.addEventListener('click', reset);
 resultButton.addEventListener('click', showResult);
-
+switchButtons();
