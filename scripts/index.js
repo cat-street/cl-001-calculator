15aab826def27bc3bb5d1c2ba2f8a80389bf7e25
Andrey Kudryavtsev <lomastering@gmail.com>
2020-06-26 13:03:07 +0300

    feat: functions

--- scripts/index.js
+++ scripts/index.js
@@ -9,68 +9,110 @@ let equalsPressed = false;
 let secondValue = null;
 
 // Return the number currently on screen
-const getOutput = () => +currentOutput.value;
+const getOutput = () => currentOutput.value;
 // Convert & set the number on screen
 const setOutput = value =>
   (currentOutput.value = value.toString().slice(0, 24));
 
 
 const printNumber = value => {
-  // New calculation after pressing 'EQUALS'
-  if (equalsPressed) reset();
+  let output = getOutput();
+  // Reset after pressing 'EQUALS' || on error
+  if (equalsPressed || output === 'Error') {
+    reset();
+    output = '';
+  }
   // If operator was pressed clear the screen
   if (currentOperator) {
     operator = currentOperator;
     currentOperator = null;
-    setOutput('');
+    output = '';
+    // setOutput('');
   }
+  // Only one decimal point allowed, removing leading zero
+  if (value === '.') {
+    if (output.indexOf('.') !== -1) return;
+  } else if (output === '0') output = '';
   // Print the number if typed
-  if (getOutput() === 0) currentOutput.value = value;
-  else currentOutput.value += value;
+  setOutput(output += value);
 }
 
 const addOperator = operation => {
-  // Prevent logging of multiple operation pressing
-  if (currentOperator) return;
+  // Prevent logging of multiple operation pressing || operations on error
+  if (currentOperator || getOutput() === 'Error') return;
   // Ability to continue calculation after 'EQUALS' operation
   if (equalsPressed) operator = null;
   // Continue calculation without 'EQUALS' pressed
-  if (operator) calculateResult();
+  if (operator) calculateResultHandler();
   // Clear 'calculation complete' flag
   equalsPressed = false;
   // Add to the log
-  result.push(getOutput(), operation);
+  result.push(+getOutput(), operation);
   // Save the operator for the calculation
   currentOperator = operation;
 }
 
-const calculateResult = () => {
+const calculateResultHandler = () => {
   // Memorize result if 'EQUALS' pressed again
-  if (equalsPressed) result.push(getOutput(), operator);
+  if (equalsPressed) result.push(+getOutput(), operator);
   // Setting up values for the calculation
-  else secondValue = parseFloat(getOutput());
-  const firstValue = parseFloat(result[result.length - 2]);
-  let resultValue = null;
-  switch (operator) {
-    case 'ADD':
-      resultValue = firstValue + secondValue;
-      break;
-    case 'SUBTRACT':
-      resultValue = firstValue - secondValue;
-      break;
-    case 'MULTIPLY':
-      resultValue = +math.multiply(math.bignumber(firstValue), math.bignumber(secondValue));
-      break;
-    case 'DIVIDE':
-      resultValue = +math.divide(math.bignumber(firstValue), math.bignumber(secondValue));
-      break;
+  else secondValue = +getOutput();
+  const firstValue = result[result.length - 2];
+  // Forbid dividing by zero
+  if (operator === 'DIVIDE' && secondValue === 0) {
+    setOutput('Error');
+    return;
   }
+  let resultValue = calculateResult(operator, firstValue, secondValue);
   result.push(secondValue, 'EQUALS');
   // Setting 'calculation complete' flag
   equalsPressed = true;
   console.log(result);
   setOutput(resultValue);
 };
+
+const calculateResult = (operation, firstValue, secondValue) => {
+  switch (operation) {
+    case 'ADD':
+      return +math.add(math.bignumber(firstValue), math.bignumber(secondValue));
+    case 'SUBTRACT':
+      return +math.subtract(math.bignumber(firstValue), math.bignumber(secondValue));
+    case 'MULTIPLY':
+      return +math.multiply(math.bignumber(firstValue), math.bignumber(secondValue));
+    case 'DIVIDE':
+      return +math.divide(math.bignumber(firstValue), math.bignumber(secondValue));
+  }
+}
+
+const addFunction = func => {
+  let output = getOutput();
+  if (output === 'Error' || output === '0') return;
+  switch (func) {
+    case 'NEGATE':
+      output = -output;
+      break;
+    case 'SQUARE':
+      // Prevent for negative numbers
+      if (+output >= 0) {
+        //TODO Write to log
+        output = +math.sqrt(math.bignumber(output));
+      } else {
+        output = 'Error';
+        return;
+      }
+    case 'PERCENT':
+      output = +math.multiply(math.bignumber(output), 0.01);
+      break;
+    case 'BACKSPACE':
+      // Prevent deleting when calculation is in progress
+      if (currentOperator) return;
+      if (output.length > 1) {
+        output = output.slice(0, -1);
+      } else output = 0;
+      break;
+  }
+  setOutput(output);
+}
 
 const buttonHandler = (type, value) => {
   switch (type) {
@@ -82,11 +124,14 @@ const buttonHandler = (type, value) => {
       break;
     case 'equals':
       // If the calculation is in progress
-      if (operator) calculateResult();
+      if (operator) calculateResultHandler();
       break;
     case 'clear':
       reset();
       break
+    case 'function':
+      addFunction(value);
+      break;
   }
 }
 
@@ -97,6 +142,7 @@ const reset = () => {
   secondValue = null;
   result.length = 0;
   setOutput(0);
+  console.clear();
 };
 
 for (const element of buttons) {
